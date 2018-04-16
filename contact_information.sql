82a2624287bfffdd1583b0e055563b30f608bf94
Matias Ariel Carena <35785244+matiascarena@users.noreply.github.com>
2018-04-16 19:47:55 -0300

    Update and rename contact_information_client.sql to contact_information.sql

--- contact_information.sql
+++ contact_information.sql
@@ -1,11 +1,11 @@
 
-CREATE OR REPLACE FUNCTION contact_information_client (
+CREATE OR REPLACE FUNCTION contact_information (
 	IN p_name                     text,
 	IN p_id                       text,
 	IN p_email                    text, 
 	IN p_address                  text,
 
-) RETURNS contact_information_client AS 
+) RETURNS contact_information AS 
 $$
 	SELECT (
   p_name,
@@ -13,5 +13,5 @@ $$
   p_email
   p_address
   
-	)::contact_information_client;
+	)::contact_information,
 $$ LANGUAGE sql STABLE STRICT;
